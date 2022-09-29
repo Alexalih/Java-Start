4372e72f91f9b1dd3b91d202d8f10de6d9d7ab99
Alexlih <sasha-zba@mil.ru>
2022-09-29 16:35:52 +0300

    Calculator

---
+++ src/main/java/Lesson8/Homework8.java
@@ -0,0 +1,7 @@
+package Lesson8;
+
+public class Homework8 {
+    public static void main(String[] args) {
+        ApplicationForm calc = new ApplicationForm("Calculator 1.0.0");
+    }
+}
---
+++ src/main/java/Lesson8/components/OperationJButton.java
@@ -0,0 +1,14 @@
+package Lesson8.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class OperationJButton extends JButton {
+
+    public OperationJButton(String text){
+        super(text);
+        setFont(new Font("TimesRoman", Font.PLAIN, 30));
+        setBackground(new Color(198, 222, 125));
+    }
+
+}
